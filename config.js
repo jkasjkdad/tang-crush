5f643c5b5c91f488aa567f6f90837a83a6d7b89f
jkasjkdad <97726913+jkasjkdad@users.noreply.github.com>
2022-01-14 15:25:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'DƯN à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao DƯN cứ không chịu ăn cơm =.=`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'nấu cơm ăn đi 😙',
+    desc: 'nấu cơm ăn đi đừng lười ',
+    btnYes: 'ăn cơm <33',
+    btnNo: 'Không ăn cơm:3',
+    question:'đúng gòi đó ăn cơm đi <3',
+    btnReply: 'Gửi cho dưn <3',
+    reply: 'ăn CƠM điiii <33333333',
+    mess: 'NẤU Cơm ăn ĐI',
+    messDesc: 'đừng ăn mì nữa nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
